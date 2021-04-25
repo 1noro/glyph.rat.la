4248439d9ba880efc6096f6afd6eb644f6552966
1noro <ppuubblliicc@protonmail.com>
2021-04-25 20:21:47 +0200

    getSquareCharTd test OK

--- __tests__/unit/table.test.js
+++ __tests__/unit/table.test.js
@@ -13,7 +13,23 @@ describe('getSquareCharTd test', () => {
         const tdB = getSquareCharTd(text);
 
         expect(tdB.innerText).toBe(tdA.innerText);
+        expect(tdB).toEqual(tdA);
+    });
+
+    test('¿tdA == tdB before click?', () => {
+        const text = 'A';
+        const input = document.createElement('input');
+
+        const tdA = document.createElement('td');
+        tdA.innerText = text;
+        tdA.style.color = 'red';
+        tdA.style.fontWeight = 'bold';
+
+        const tdB = getSquareCharTd(text, input);
         tdB.click();
+
+        expect(tdB.innerText).toBe(tdA.innerText);
+        expect(input.value).toBe(text);
         expect(tdB).toEqual(tdA);
     });
 });
